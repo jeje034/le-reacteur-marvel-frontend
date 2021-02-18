bded11aa2e3402c3b2dee5ad78c3c409236e2036
Jeje S <54934028+jeje034@users.noreply.github.com>
2021-02-18 13:10:46 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../assets/langfr-1920px-MarvelLogo.svg_uw9pi8.png";
+
+const Header = () => {
+    return (
+        <div>
+            <img className="header-logo" alt="Logo" src={logo} />
+        </div>
+    );
+};
+
+export default Header;
