eb59a7c9d3850cad8b493470a0aaadd6800c30bb
Jeje S <54934028+jeje034@users.noreply.github.com>
2021-02-21 23:40:43 +0100

    CSS for navigation bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,8 +3,18 @@ import "./NavigationBar.scss";
 const NavigationBar = ({ changePageFunction }) => {
     return (
         <div className="navigation-bar-main">
-            <button onClick={() => changePageFunction(-1)}>Précédent</button>
-            <button onClick={() => changePageFunction(+1)}>Suivant</button>
+            <button
+                className="navigation-bar-button navigation-bar-button-previous"
+                onClick={() => changePageFunction(-1)}
+            >
+                Précédent
+            </button>
+            <button
+                className="navigation-bar-button navigation-bar-button-next"
+                onClick={() => changePageFunction(+1)}
+            >
+                Suivant
+            </button>
         </div>
     );
 };
